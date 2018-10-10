0d1a81656492793fdf9e28ae12fe50b3b41fce8a
yushanshan <1284661938@qq.com>
2018-10-10 15:37:26 +0800

    test

---
+++ Stu_tools/stu.py
@@ -0,0 +1,24 @@
+# -*-coding: utf-8-*-
+
+# **************************file desc*****************************
+__author__ = 'yushanshan'
+# createTime : 2018/10/10 15:34
+# desc : this is new py file, please write your desc for this file
+# ****************************************************************
+
+class stu():
+
+
+        def __init__(self):
+            pass
+
+        def search(self):
+            str = "姗姗来迟"
+            print(str)
+
+
+
+
+if __name__ == '__main__':
+    stu = stu()
+    stu.search()
